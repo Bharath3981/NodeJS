17e1e45fd28160d04cc113090119e2a4e76a9477
Bharath3981 <bharath.baisetty@gmail.com>
2024-02-23 21:32:55 +0530

    Implemented get Request

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import { readFileSync } from 'node:fs';
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Welcome to express');
+const tours = JSON.parse(
+  readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: { tours },
+  });
 });
 app.listen(8000, () => {
-  console.log('SErvier started');
+  console.log('Server started');
 });
