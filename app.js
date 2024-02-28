f2b7f37eda9e5a48858fb5ed4206f98b9401e479
Bharath3981 <bharath.baisetty@gmail.com>
2024-02-28 07:35:46 +0530

    Serving static files

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ export const app = express();
 //MIddelwares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
